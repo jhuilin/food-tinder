bdcdc24fdf5a7846b458588cb11d50a3364deb49
jianhui <jianhuilin1124@gmail.com>
2020-06-14 14:05:28 -0400

    add map and navigtor

--- Navigation/StackNavigation.js
+++ Navigation/StackNavigation.js
@@ -4,15 +4,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import SwipeScreen from "../screens/swipeScreen/SwipeScreen";
 import NoImages from "../screens/swipeScreen/NoImages";
+import MainMap from "../screens/Map/MainMap";
 
 const Stack = createStackNavigator();
 
 const MainStackNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" headerMode="none" mode="modal">
+      <Stack.Navigator
+        initialRouteName="MainMap"
+        headerMode="none"
+        mode="modal"
+      >
         <Stack.Screen name="NoImages" component={NoImages} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MainMap" component={MainMap} />
         <Stack.Screen name="Swipe" component={SwipeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
